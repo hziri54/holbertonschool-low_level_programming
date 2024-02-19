ded28f553f7ca7e0d387d8ac7d47f17e99ebb32f
hziri54 <zirihouamdi64@gmail.com>
2024-02-19 13:34:31 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("\n %d octets pour variable de type char ",sizeof(char));
 	printf("\n %d octets pour variable de type int ",sizeof(int));
 	printf("\n %d octets pour variable de type long int ",sizeof(long int));
-	printf("\n %d octets pour variable de type float ",sizeof(float))
 	printf("\n %d octets pour variable de type float ",sizeof(long long int));
+	printf("\n %d octets pour variable de type float ",sizeof(float))
 	return (0);
 }

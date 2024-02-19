d60b70adec8bdad95dfb78cddd50b652a550c0c1
hziri54 <zirihouamdi64@gmail.com>
2024-02-19 13:06:38 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Description: This function prints a phrase using puts.
+i *
+ *Return: Always returns 0 for success
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}

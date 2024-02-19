418e9a07b05003a6c90924872ced57b562db4867
hziri54 <zirihouamdi64@gmail.com>
2024-02-19 10:31:35 +0000

    4-puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *	Return: Always returns 0 for success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

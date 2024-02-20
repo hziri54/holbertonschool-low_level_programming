716a300bb56f3682e6ce60a6d72a1767f152b958
root <zirihouamdi64@gmail.com>
2024-02-20 13:00:18 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 for success.
+ */
+
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet <= 'a')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}

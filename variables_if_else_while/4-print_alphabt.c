843c8eb09d3249365c75bab91155e53a03062a70
root <zirihouamdi64@gmail.com>
2024-02-20 10:17:40 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: 0 for success.
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

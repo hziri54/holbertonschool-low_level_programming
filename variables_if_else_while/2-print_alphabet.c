bd9e3c3ee27f34653d9bb3f293de1d118a68ccf7
root <zirihouamdi64@gmail.com>
2024-02-20 09:45:20 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 for success.
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}

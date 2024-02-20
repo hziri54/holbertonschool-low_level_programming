42a074b415340e36fb83523d77c442393b480f49
root <zirihouamdi64@gmail.com>
2024-02-20 09:04:15 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,48 +1,31 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: This program will assign a random number to the variable n
- * and print the last digit of n along with specific messages.
+ * Description: print the last digit of the number stored in the variable n
  *
- * Return: Always 0 for success.
+ * Return: O for success.
  */
 
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-
-    printf("Le dernier chiffre de %d est ", n);
-
-    // Obtenez le dernier chiffre de n en prenant le reste de la division par 10
-    int lastDigit = n % 10;
-
-    // Affiche le dernier chiffre de n
-    printf("%d ", lastDigit);
-
-    printf("et est ");
-
-    // Applique les conditions demandées
-    if (lastDigit > 5)
-    {
-        printf("supérieur à 5");
-    }
-    else if (lastDigit == 0)
-    {
-        printf("0");
-    }
-    else
-    {
-        printf("inférieur à 6 et différent de 0");
-    }
-
-    printf("\n");
-
-    return (0);
-} 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d", n, n % 10);
+	if (n % 10 > 5)
+	{
+		printf(" and is greater than 5\n");
+	} else if (n % 10 == 0)
+	{
+		printf(" and is 0\n");
+	} else
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
+	return (0);
+}

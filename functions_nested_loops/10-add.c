bce9c605da2e784245307eca3ffd454762ee1a4b
hziri54 <zirihouamdi64@gmail.com>
2024-02-22 13:07:15 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ *
+ * @i: nombre
+ *
+ * @n: nombre
+ *
+ * Description: adds two integers and returns the result.
+ *
+ * Return: 0 for success.
+ */
+
+int add(int i, int n)
+
+{
+	int r = i + n;
+
+	return (r);
+}

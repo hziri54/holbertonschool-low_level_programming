ef19b91a221bbe30212cd679dccee6e7dbe9ddc7
hziri54 <zirihouamdi64@gmail.com>
2024-02-26 09:20:18 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isupper - entry point 
+ *
+ * @c: letter 
+ *
+ * Description: cheks for uppercase character 
+ *
+ * Return: 0 for sucess.
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	} { 
+		return (0);
+	}
+}

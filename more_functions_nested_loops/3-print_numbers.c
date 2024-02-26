6b4d332ee65274d95db0063225bb113e31a5c8a2
hziri54 <zirihouamdi64@gmail.com>
2024-02-26 10:28:52 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,2 +1,20 @@
 #include "main.h"
 
+/**
+ * print_numbers - entry point
+ *
+ * Description: prints the numbers from 0 to 9
+ *
+ * Return: 0 for success.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}

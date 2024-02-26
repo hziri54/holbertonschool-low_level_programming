aed93929237e1a24ce8556ca36d68c639009467b
hziri54 <zirihouamdi64@gmail.com>
2024-02-26 09:58:21 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _mul - entry point 
+ *
+ * @a: number
+ * @b: number
+ *
+ * Description: to multiply two numbers
+ *
+ * Return 0 for sucess 
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

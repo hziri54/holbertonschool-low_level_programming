060d2a69592d7140a87323d34e0ac4bfab1d5bd0
hziri5 <zirihouamdi64@gmail.com>
2024-02-26 21:08:53 +0000

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ *
+ * @size: taille
+ *
+ * Descrition: prints a triangle, followed by a new line
+ *
+ * Return: 0 for success.
+ */
+
+void print_triangle(int size)
+{
+	int count;
+	int space;
+
+	if (size > 0)
+	{
+		for (count = 0; count < size; count++)
+		{
+			for (space = size - count; space > 1 ; space--)
+			{
+				_putchar(' ');
+			}
+			for (space = 0; space <= count; space++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+}

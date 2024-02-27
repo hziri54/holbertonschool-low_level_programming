31f72b3981de64e0add1d057b53c4239930d7ce0
hziri54 <zirihouamdi64@gmail.com>
2024-02-27 09:23:37 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap - entry point 
+ *
+ * Description: exchange the values of 2 integers
+ *
+ * Return: 0 for sucess.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temporary;
+
+	temporary = *a;
+	*a + *b;
+	temporary = *b;
+}

d69ba7528e6eeb9f4b4ecb52e063a1eaf63e2e3b
hziri54 <zirihouamdi64@gmail.com>
2024-02-27 09:47:43 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * strlen_int - entry point
+ *
+ * @s: string thr returns
+ *
+ * Description: return the length of a character string
+ *
+ * Return: 0 for sucess
+ */
+
+int _strlen(char *s)
+{
+

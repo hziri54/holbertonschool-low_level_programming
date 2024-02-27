6508beff913a919df5be0fd12cf607b2c8f1a00a
hziri54 <zirihouamdi64@gmail.com>
2024-02-27 09:14:44 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point 
+ *
+ * @n: value
+ *
+ * Description: use a pointer
+ *
+ * Return: 0 for sucess.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

96d118cb39403840108ca492ea6e0d05f8a869c8
hziri54 <zirihouamdi64@gmail.com>
2024-03-03 20:51:48 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,23 +3,20 @@
 /**
  *
  * print_rev - entry point
- *
  * @s: string
- *
  * Description: write a character string that prints backwards
- *
- * Return: 0 for success. 
+ * Return: 0 for success.
  **/
 
 void print_rev(char *s)
 {
 	int len = 0;
 	int i = 0;
-       
+	
 	while (s[len])
 	{
 		len++;
-	{	
+	{
 	
 	i = len - 1;
 	while (i >= 0)

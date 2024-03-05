2d0a567aab9be2f29fb76602dcfdd8dba1d0d785
hziri54 <zirihouamdi64@gmail.com>
2024-03-05 13:08:21 +0000

    4-strpbrk.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,3 +20,4 @@ char *leet(char *);
 void print_rev(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);

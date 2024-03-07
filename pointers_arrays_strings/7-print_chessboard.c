c16d4cb44ac496da0559f8ae1d01221fc1e06324
hziri54 <zirihouamdi64@gmail.com>
2024-03-07 13:23:09 +0000

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,25 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_chessboard - Print the chesboard
- * @: array of pieces
- *
- * Return: Nothing
- */
-void void print_chessboard(char (*a)[8]
+  
+print_chessboard - Print the chessboard
+@a: array of pieces
+*
+Return: Nothing.
+*/
+void print_chessboard(char (*a)[8])
 {
-	int b, c;
+    int b, c;
 
-	for (b = 0;  b < 8; b+++)
-	{
-		for (c = 0; c < 8; c++)
-		{
-			_putchar(a[b][c];
-		}
+    for (b = 0; b < 8; b++)
+    {
+        for (c = 0; c < 8; c++)
+        {
+            _putchar(a[b][c]);
+        }
 
-		_putchar('\n');
-	}
+        _putchar('\n');
+    }
 }
+﻿
+

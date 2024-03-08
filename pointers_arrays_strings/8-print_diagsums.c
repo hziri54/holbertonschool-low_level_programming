bd0322d626e4d95db3c11b098867bfdf11955de0
hziri54 <zirihouamdi64@gmail.com>
2024-03-08 16:49:01 +0000

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,28 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 /**
- 
-print_diagsums - Write a function that prints the sum of the two diagonals.*
-Return: ..
-*
-@a: int
-*
-@size: int
-*/
+ * print_diagsums - Write a function that prints the sum of the two diagonals.
+ *
+ * Return: ..
+ *
+ * @a: int
+ *
+ * @size: int
+ */
 void print_diagsums(int *a, int size)
 {
-    int b, y = 0, z = 0;
+	int b, y = 0, z = 0;
 
-    for (b = 0; b < size; b++)
-    {
-        y += a[b];
-        a += size;
-    }
-    a -= size;
-    for (b = 0; b < size; b++)
-    {
-        z += a[b];
-        a -= size;
-    }
-    printf("%d, %d\n", y, z);
+	for (b = 0; b < size; b++)
+	{
+		y += a[b];
+		a += size;
+	}
+	a -= size;
+	for (b = 0; b < size; b++)
+	{
+		z += a[b];
+		a -= size;
+	}
+	printf("%d, %d\n", y, z);
 }

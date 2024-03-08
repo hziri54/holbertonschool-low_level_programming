79c4ace83827e4863f17af20d7cf4ab152d77edb
hziri54 <zirihouamdi64@gmail.com>
2024-03-08 10:14:50 +0000

    putchar dans main.h

--- recursion/main.h
+++ recursion/main.h
@@ -1,7 +1,14 @@
+#ifndef MAIN
+#define MAIN
+
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt(int n, int a);
+int prime(int n, int a);
 int is_prime_number(int n);
+#endif

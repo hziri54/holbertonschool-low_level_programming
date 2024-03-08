32d51cd55ce551df78e99801b583ab46fc39d5d8
hziri54 <zirihouamdi64@gmail.com>
2024-03-08 16:50:24 +0000

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- 
-_strchr - locate c*
-Return: NULL
-*
-@s: char
-*
-@c: char
-*/
-char _strchr(chars, char c)
+ * _strchr - locate c
+ *
+ * Return: NULL
+ *
+ * @s: char
+ *
+ * @c: char
+ */
+char *_strchr(char *s, char c)
 {
-    int a, b;
+	int a, b;
 
-    for (a = 0; s[a]; a++)
-    {
-    }
-    for (b = 0; b <= a; b++)
-    {
-        if (c == s[b])
-        {
-            s += b;
-            return (s);
-        }
-    }
+	for (a = 0; s[a]; a++)
+	{
+	}
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
 
-    return ('\0');
+	return ('\0');
 }
